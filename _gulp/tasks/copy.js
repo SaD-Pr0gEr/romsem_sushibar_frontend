9512945b1cd666d7547f9ed01b337bfe595cf567
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2024-11-18 19:50:11 +0500

    cart

--- _gulp/tasks/copy.js
+++ _gulp/tasks/copy.js
@@ -1,4 +1,4 @@
 export async function copyStatic() {
-    app.gulp.src(app.paths.src.copyStaticFiles)
+    app.gulp.src(app.paths.src.copyStaticFiles, {encoding: false})
         .pipe(app.gulp.dest(app.paths.build.assetsDir))
 }
